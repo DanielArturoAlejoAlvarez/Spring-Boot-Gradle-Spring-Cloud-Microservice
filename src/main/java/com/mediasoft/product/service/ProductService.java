f23243f6113ac8f6127d72451091268ce5297422
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2020-12-19 19:13:53 -0500

    Implement service methods

--- src/main/java/com/mediasoft/product/service/ProductService.java
+++ src/main/java/com/mediasoft/product/service/ProductService.java
@@ -2,44 +2,74 @@ package com.mediasoft.product.service;
 
 import com.mediasoft.product.entity.Category;
 import com.mediasoft.product.entity.Product;
+import com.mediasoft.product.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class ProductService implements IProductService{
+    @Autowired
+    private ProductRepository repo;
+
     @Override
     public List<Product> listAllProduct() {
-        return null;
+        return repo.findAll();
     }
 
     @Override
     public Product getProduct(Long id) {
-        return null;
+        return repo.findById(id).orElse(null);
     }
 
     @Override
     public Product createProduct(Product p) {
-        return null;
+        p.setStatus("CREATED");
+        p.setCreatedAt(new Date());
+        return repo.save(p);
     }
 
     @Override
     public Product updateProduct(Product p) {
-        return null;
+        Product prodDB = getProduct(p.getId());
+        if (null==prodDB) {
+            return null;
+        }
+        prodDB.setName(p.getName());
+        prodDB.setDescription(p.getDescription());
+        prodDB.setPrice(p.getPrice());
+        prodDB.setImage(p.getImage());
+        prodDB.setCategory(p.getCategory());
+
+        return repo.save(prodDB);
     }
 
     @Override
     public Product deleteProduct(Long id) {
-        return null;
+        Product prodDB = getProduct(id);
+        if (null==prodDB) {
+            return null;
+        }
+        prodDB.setStatus("DELETED");
+
+        return repo.save(prodDB);
     }
 
     @Override
     public List<Product> findByCategory(Category c) {
-        return null;
+        return repo.findByCategory(c);
     }
 
     @Override
     public Product updStock(Long id, Double qty) {
-        return null;
+        Product prodDB = getProduct(id);
+        if (null==prodDB) {
+            return null;
+        }
+        Double stock = prodDB.getStock() - qty;
+        prodDB.setStock(stock);
+        return repo.save(prodDB);
     }
 }
