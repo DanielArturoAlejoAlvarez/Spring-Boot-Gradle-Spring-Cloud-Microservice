b0f894b29cdb5fc9f5e6e841ad52d7b10f350935
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2020-12-21 14:06:17 -0500

    Completing the validation for our product model

--- src/main/java/com/mediasoft/product/entity/Product.java
+++ src/main/java/com/mediasoft/product/entity/Product.java
@@ -7,7 +7,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import java.util.Date;
 
 @Entity
@@ -20,25 +22,34 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotEmpty(message = "The name field cannot be empty!")
     @Column(nullable = false)
     private String name;
+
     private String description;
+
+    @Positive(message = "The stock field must be greater than zero")
     private Double stock;
+
     @Column(name = "price", nullable = false)
     private Double price;
+
     @Column(name = "image", length = 512)
     private String image;
+
     private String status;
+
     @Column(name = "created_at")
     @Temporal(TemporalType.TIMESTAMP)
     private Date createdAt;
+
     @NotNull(message = "The field name must not be null or empty")
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id")
     @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
     private Category category;
+
     @Column(name = "code", nullable = false, unique = true)
     private String code;
-
 
 }
