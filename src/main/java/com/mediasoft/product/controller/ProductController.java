3e52ad6c93ad8b8c43ab167b7179427117ed540b
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2020-12-21 12:03:30 -0500

    Adding method to get product by id in Rest controller

--- src/main/java/com/mediasoft/product/controller/ProductController.java
+++ src/main/java/com/mediasoft/product/controller/ProductController.java
@@ -5,10 +5,7 @@ import com.mediasoft.product.entity.Product;
 import com.mediasoft.product.service.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -44,6 +41,16 @@ public class ProductController {
         }
         return ResponseEntity.ok(products);
     }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable("id") Long id) {
+        Product product = service.getProduct(id);
+        if (null==product) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(product);
+    }
+
 
 
 }
