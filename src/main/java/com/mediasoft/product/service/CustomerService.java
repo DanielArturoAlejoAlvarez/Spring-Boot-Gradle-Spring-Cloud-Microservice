3798731c1077fef722ac11993a0e23ac47508ce0
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2020-12-22 13:29:56 -0500

    Implement our customer service

---
+++ src/main/java/com/mediasoft/product/service/CustomerService.java
@@ -0,0 +1,38 @@
+package com.mediasoft.product.service;
+
+import com.mediasoft.product.entity.Customer;
+import com.mediasoft.product.entity.Region;
+
+import java.util.List;
+
+public class CustomerService implements ICustomerService{
+    @Override
+    public List<Customer> listAllCustomer() {
+        return null;
+    }
+
+    @Override
+    public List<Customer> listCustomerByRegion(Region region) {
+        return null;
+    }
+
+    @Override
+    public Customer createCustomer(Customer customer) {
+        return null;
+    }
+
+    @Override
+    public Customer updateCustomer(Customer customer) {
+        return null;
+    }
+
+    @Override
+    public Customer deleteCustomer(Customer customer) {
+        return null;
+    }
+
+    @Override
+    public Customer getCustomer(Long id) {
+        return null;
+    }
+}
