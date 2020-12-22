6bd032c19f9e5e997c08393db08b4ed8a8054919
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2020-12-22 13:55:00 -0500

    Adding the different methods for our service

--- src/main/java/com/mediasoft/product/service/CustomerService.java
+++ src/main/java/com/mediasoft/product/service/CustomerService.java
@@ -2,37 +2,65 @@ package com.mediasoft.product.service;
 
 import com.mediasoft.product.entity.Customer;
 import com.mediasoft.product.entity.Region;
+import com.mediasoft.product.repository.CustomerRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
+@Service
 public class CustomerService implements ICustomerService{
+    @Autowired
+    private CustomerRepository repo;
+
     @Override
     public List<Customer> listAllCustomer() {
-        return null;
+        return repo.findAll();
     }
 
     @Override
     public List<Customer> listCustomerByRegion(Region region) {
-        return null;
+        return repo.findByRegion(region);
     }
 
     @Override
     public Customer createCustomer(Customer customer) {
-        return null;
+        Customer customerDB = repo.findByNumberID(customer.getNumberID());
+        if (customerDB != null) {
+            return customerDB;
+        }
+        customer.setStatus("CREATED");
+        customerDB = repo.save(customer);
+        return customerDB;
     }
 
     @Override
     public Customer updateCustomer(Customer customer) {
-        return null;
+        Customer customerDB = getCustomer(customer.getId());
+        if (null==customerDB) {
+            return null;
+        }
+        customerDB.setFirstName(customer.getFirstName());
+        customerDB.setLastName(customer.getLastName());
+        customerDB.setEmail(customer.getEmail());
+        customerDB.setPhotoURL(customer.getPhotoURL());
+        return repo.save(customerDB);
     }
 
     @Override
     public Customer deleteCustomer(Customer customer) {
-        return null;
+        Customer customerDB = getCustomer(customer.getId());
+        if (null==customerDB) {
+            return null;
+        }
+        customer.setStatus("DELETED");
+        return repo.save(customer);
     }
 
     @Override
     public Customer getCustomer(Long id) {
-        return null;
+        return repo.findById(id).orElse(null);
     }
 }
