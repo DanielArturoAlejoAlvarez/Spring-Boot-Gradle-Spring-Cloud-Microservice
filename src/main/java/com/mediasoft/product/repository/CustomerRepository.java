a73823c8fd375d90d2c3fee077f166914ed0b7ea
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2020-12-22 13:15:45 -0500

    Creating customer repository

---
+++ src/main/java/com/mediasoft/product/repository/CustomerRepository.java
@@ -0,0 +1,15 @@
+package com.mediasoft.product.repository;
+
+import com.mediasoft.product.entity.Customer;
+import com.mediasoft.product.entity.Region;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer,Long> {
+    public Customer findByNumberID(String numberID);
+    public List<Customer> findByLastName(String lastName);
+    public List<Customer> findByRegion(Region region);
+}
