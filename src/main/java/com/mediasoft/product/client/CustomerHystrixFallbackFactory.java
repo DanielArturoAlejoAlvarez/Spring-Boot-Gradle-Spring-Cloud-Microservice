90f19de3bc157e78d0763f3409ebc81bf30ec72b
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2020-12-23 11:26:44 -0500

    Implement component CustomerHystrixFallbackFactory

--- src/main/java/com/mediasoft/product/client/CustomerHystrixFallbackFactory.java
+++ src/main/java/com/mediasoft/product/client/CustomerHystrixFallbackFactory.java
@@ -1,4 +1,20 @@
 package com.mediasoft.product.client;
 
-blic class CustomerHystrixFallbackFactory {
+import com.mediasoft.product.models.Customer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomerHystrixFallbackFactory implements CustomerClient {
+
+    @Override
+    public ResponseEntity<Customer> getCustomer(Long id) {
+        Customer customer = Customer.builder()
+                .firstName("none")
+                .lastName("none")
+                .email("none")
+                .photoUrl("none")
+                .build();
+        return ResponseEntity.ok(customer);
+    }
 }
