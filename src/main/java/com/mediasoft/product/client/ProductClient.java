a0c46aa56d5fe048bfe8b68195e98f3ebd98418a
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2020-12-23 12:03:20 -0500

    Creating interface Product Client

--- src/main/java/com/mediasoft/product/client/ProductClient.java
+++ src/main/java/com/mediasoft/product/client/ProductClient.java
@@ -1,4 +1,20 @@
 package com.mediasoft.product.client;
 
+import com.mediasoft.product.models.Product;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient("product-service")
+@RequestMapping(value = "/'products")
 public interface ProductClient {
+
+    @GetMapping(name = "/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable("id") Long id);
+
+    @GetMapping(name = "/{id}/stock")
+    public ResponseEntity<Product> updStockProduct(@PathVariable("id") Long id, @RequestParam(name = "qty", required = true) Double qty);
 }
