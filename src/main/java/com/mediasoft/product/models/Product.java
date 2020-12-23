bb3cb50070db60e9b1e53c53a07046ed4d46d98d
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2020-12-22 19:56:26 -0500

    Creating model product

--- src/main/java/com/mediasoft/product/models/Product.java
+++ src/main/java/com/mediasoft/product/models/Product.java
@@ -1,4 +1,14 @@
 package com.mediasoft.product.models;
 
+import lombok.Data;
+
+@Data
 public class Product {
+    private Long id;
+    private String name;
+    private String description;
+    private Double stock;
+    private Double price;
+    private String status;
+    private Category category;
 }
